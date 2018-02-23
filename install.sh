4742dcdde4430df9e4a5721a0d0f6859eec8af65
Kirill Salykin <kirill.salykin@gmail.com>
2018-02-23 10:24:57 +0100

    Add global gitignore

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-ln -nfs "$(pwd)/.gitconfig"  "$HOME/.gitconfig"
-ln -nfs "$(pwd)/.zshrc"      "$HOME/.zshrc"
-ln -nfs "$(pwd)/.spacemacs"  "$HOME/.spacemacs"
+ln -nfs "$(pwd)/.gitignore_global"  "$HOME/.gitignore_global"
+ln -nfs "$(pwd)/.gitconfig"         "$HOME/.gitconfig"
+ln -nfs "$(pwd)/.zshrc"             "$HOME/.zshrc"
+ln -nfs "$(pwd)/.spacemacs"         "$HOME/.spacemacs"
