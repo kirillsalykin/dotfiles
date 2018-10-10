f8ef09d033297bb3e512bc808eddec807e719399
Kirill Salykin <kirill.salykin@gmail.com>
2018-10-10 14:33:39 +0200

    Add editorconfig

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 ln -nfs "$(pwd)/.gitignore_global"  "$HOME/.gitignore_global"
 ln -nfs "$(pwd)/.gitconfig"         "$HOME/.gitconfig"
 ln -nfs "$(pwd)/.zshrc"             "$HOME/.zshrc"
+ln -nfs "$(pwd)/.editorconfig"      "$HOME/.editorconfig"
 ln -nfs "$(pwd)/.spacemacs"         "$HOME/.spacemacs"
