c060ee404b453388731bfcab14948a9f6a34d759
Kirill Salykin <kirill.salykin@gmail.com>
2024-06-12 14:55:19 +0200

    Realign things

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-ln -nfs "$(pwd)/.gitignore_global"  "$HOME/.gitignore_global"
-ln -nfs "$(pwd)/.gitconfig"         "$HOME/.gitconfig"
-ln -nfs "$(pwd)/.zshrc"             "$HOME/.zshrc"
-ln -nfs "$(pwd)/.editorconfig"      "$HOME/.editorconfig"
-ln -nfs "$(pwd)/nvchad.custom"      "$HOME/.config/nvim/lua/custom"
+ln -nfs $(pwd)/.gitignore_global $HOME/.gitignore_global
+ln -nfs $(pwd)/.gitconfig        $HOME/.gitconfig
+ln -nfs $(pwd)/.zshrc            $HOME/.zshrc
+ln -nfs $(pwd)/.editorconfig     $HOME/.editorconfig
+ln -nfs $(pwd)/nvchad.custom     $HOME/.config/nvim/lua/custom
 
 source ./osx
 
